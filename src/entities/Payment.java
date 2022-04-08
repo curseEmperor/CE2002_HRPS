9a5840da42455ccde6853cc8908312d284911777
uztra4 <92169451+uztra4@users.noreply.github.com>
2022-04-09 03:05:14 +0800

    Add files via upload

---
+++ src/entities/Payment.java
@@ -0,0 +1,122 @@
+package entities;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.io.Serializable;
+
+
+public class Payment implements Serializable
+{
+    private int PaymentID;
+    private int roomID;
+    private ArrayList<Order> OrderList; //store all orderIDs for a roomID, should be from room
+    private boolean paymentStatus; //paid or unpaid (true = paid, false = unpaid)
+    private String date;
+    private double total = 0;
+    SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss");
+
+
+    //Class Constructor
+    public Payment(int PaymentID, int roomID)
+    {
+        this.PaymentID = PaymentID;
+        this.OrderList = new ArrayList<Order>();  //create empty order list
+        this.paymentStatus = false;
+        this.roomID = roomID;
+        Calendar c = Calendar.getInstance();
+        String d = sdf.format(c.getTime());
+        this.date = d;
+        PaymentID ++;
+    }
+
+
+    ////////////////////////Setters and Getters///////////////////////////////
+    public int getPaymentID()
+    {
+        return PaymentID;
+    }
+
+    public void setPaymentID(int PaymentID)
+    {
+        this.PaymentID = PaymentID;
+    }
+
+    public boolean getpaymentStatus()
+    {
+        return paymentStatus;
+    }
+
+    public void setPaymentStatus(boolean paymentStatus)
+    {
+        this.paymentStatus = paymentStatus;
+    }
+
+    public int getroomID()
+    {
+        return roomID;
+    }
+
+    public void setroomID(int roomID)
+    {
+        this.roomID = roomID;
+    }
+
+    public double getTotal()
+    {
+        return total;
+    }
+
+    public void setTotal(double total)
+    {
+        this.total = total;
+    }
+
+    public String getDate()
+    {
+        return this.date;
+    }
+
+    public void setDate(String date)
+    {
+        this.date = date;
+    }
+
+    public ArrayList<Order> getOrderList()
+    {
+        return this.OrderList;
+    }  
+
+    public double calcSubTotal()
+    {
+        double subTotal = 0;
+        if (getOrderList() != null)
+        {
+            for (int i=0; i< this.OrderList.size(); i++) {
+                subTotal += this.OrderList.get(i).calcOrderPrice();
+            }
+        }
+        return subTotal;
+    }
+
+    public double calcTotal(Payment payment, int roomID)
+    {
+        double subTotal = calcSubTotal();
+        double total = subTotal * 1.07 * 1.10;
+        payment.setTotal(total);
+        return total;
+    }
+
+    public void printOrderList()
+    {
+        for (int i = 0; i<this.getOrderList().size(); i++)
+        {
+            this.OrderList.get(i).viewOrder();
+        }
+    }
+
+    public void addOrder(Order order)
+    {
+        this.OrderList.add(order);
+    }    
+}
