e1228c0bc9cac7c8e47586b54a279f9e07934628
uztra4 <92169451+uztra4@users.noreply.github.com>
2022-04-15 20:37:09 +0800

    Update GuestController.java

--- src/Controller/GuestController.java
+++ src/Controller/GuestController.java
@@ -40,6 +40,11 @@ public class GuestController implements IController {
 
         Guest guest = (Guest) entities;
         guestList.add(guest);
+        System.out.println("========================");
+        System.out.println("   Guest Details: ");
+        System.out.println("========================");
+        System.out.println(guestList.toString().replace("[", "").replace("]", ""));
+        System.out.println("========================");
 
         storeData();
     }
@@ -100,11 +105,6 @@ public class GuestController implements IController {
             out.writeInt(guestList.size());
             for (Guest guest : guestList)
                 out.writeObject(guest);
-            System.out.println("========================");
-            System.out.println("   Guest Details: ");
-            System.out.println("========================");
-            System.out.println(guestList.toString().replace("[", "").replace("]", ""));
-            System.out.println("========================");
             System.out.println("Entries Saved!");
             out.close();
         } catch (IOException e) {
