7a66ed62a69d9647b98b25dc2cf0784bc98e3049
curseEmperor <jcjs1510@gmail.com>
2022-04-17 13:10:15 +0800

    javadoc comments

--- src/Entity/Entities.java
+++ src/Entity/Entities.java
@@ -2,6 +2,12 @@ package Entity;
 
 import java.io.Serializable;
 
+/***
+ * Represents a Creditcard
+ * 
+ * @version 1.0
+ * @since 2022-04-17
+ */
 public class Entities implements Serializable {
 
 }
